059a743ca128ac3f59f93bbf465f27db2886ffba
Joseph Ting <josephting@users.noreply.github.com>
2026-09-10 01:33:57 +0900

    feat(base): Migrate asdf-vm to mise
    chore(tool-versions): Update tool versions

---
+++ scripts/base/01-install-mise.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+ENDCOLOR="\e[0m"
+YELLOW="\e[33m"
+
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+setup_mise() {
+  curl https://mise.run | sh
+
+  if ! command_exists asdf; then
+    echo "mise not found!"
+    exit 127
+  fi
+}
+
+if command_exists mise; then
+  echo "mise is installed. Skipping..."
+else
+  echo "${YELLOW}Installing mise tool manager...${ENDCOLOR}"
+  setup_mise
+  echo "mise installed!"
+fi
+
+
